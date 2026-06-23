40456b2a0d67c0907c4a383508c02ce8e15ee314
keranjan <gnn6rrzgxf@privaterelay.appleid.com>
2026-06-23 09:37:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    offline-varalla välimuisti. Supabase-kutsut, fontit ja CDN-skripti menevät
    aina suoraan verkkoon (ei välimuistia). */
 
-const CACHE = 'omatoimi-v1.4.10';
+const CACHE = 'omatoimi-v1.4.11';
 const CORE = [
   './index.html',
   './app.js',
